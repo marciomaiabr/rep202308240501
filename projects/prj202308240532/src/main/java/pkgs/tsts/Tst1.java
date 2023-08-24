39d841daa84462e02eef98bc39564d10d78ee061
Marcio Maia <marcio.luis.maia@gmail.com>
2023-08-24 10:11:23 -0400

    criando teste para o JPA

--- projects/prj202308240532/src/main/java/pkgs/tsts/Tst1.java
+++ projects/prj202308240532/src/main/java/pkgs/tsts/Tst1.java
@@ -1,5 +1,10 @@
 package pkgs.tsts;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -10,7 +15,7 @@ import pkgs.models.ObjetoX;
 public class Tst1 {
 
 	public static void main(String[] args) {
-		m2();
+		m3();
 	}
 
 	private static void m1() {
@@ -38,7 +43,7 @@ public class Tst1 {
 			t = session.beginTransaction();
 
 			objetoX = new ObjetoX();
-			objetoX.setDescricao("Teste descrição...");
+			objetoX.setDescricao("Teste com hibernate descrição...");
 
 			session.save(objetoX);
 
@@ -56,6 +61,48 @@ public class Tst1 {
 			sessionFactory.close();
 
 			System.out.println("testes com o hibernate executados com sucesso...");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void m3() {
+		System.out.println("testes com o JPA...");
+
+		ObjetoX objetoX = null;
+
+		EntityManagerFactory emf = null;
+		EntityManager em = null;
+		EntityTransaction et = null;
+
+		try {
+
+			emf = Persistence.createEntityManagerFactory("myPUCreate");
+			em = emf.createEntityManager();
+			et = em.getTransaction();
+
+			et.begin();
+
+			objetoX = new ObjetoX();
+			objetoX.setDescricao("Teste com JPA descrição...");
+
+			em.merge(objetoX);
+
+			et.commit();
+			em.close();
+			emf.close();
+
+			emf = Persistence.createEntityManagerFactory("myPUNoCreate");
+			em = emf.createEntityManager();
+
+			objetoX = em.find(ObjetoX.class, 1);
+
+			System.out.println("[objetoX=" + objetoX + "]");
+
+			em.close();
+			emf.close();
+
+			System.out.println("testes com o JPA executados com sucesso...");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
