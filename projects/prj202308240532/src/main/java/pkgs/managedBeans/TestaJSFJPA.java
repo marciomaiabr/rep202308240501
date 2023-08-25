dbbbc7afbd2c1c10d632163fb3cf433bf49c26f5
Marcio Maia <marcio.luis.maia@gmail.com>
2023-08-25 04:24:39 -0400

    criando MB TestaJSFJPA

---
+++ projects/prj202308240532/src/main/java/pkgs/managedBeans/TestaJSFJPA.java
@@ -0,0 +1,14 @@
+package pkgs.managedBeans;
+
+import javax.faces.bean.ManagedBean;
+
+import pkgs.util.Util;
+
+@ManagedBean
+public class TestaJSFJPA {
+
+	public void testaJPA() {
+		Util.testaJPA();
+	}
+	
+}
