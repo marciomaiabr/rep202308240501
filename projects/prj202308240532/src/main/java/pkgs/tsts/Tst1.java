b5745bfb5de9e248cf5db1f4b18060a087899aca
Marcio Maia <marcio.luis.maia@gmail.com>
2023-08-25 03:57:13 -0400

    Atualizando testes para apontar para a classe Util

--- projects/prj202308240532/src/main/java/pkgs/tsts/Tst1.java
+++ projects/prj202308240532/src/main/java/pkgs/tsts/Tst1.java
@@ -11,6 +11,7 @@ import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import pkgs.models.ObjetoX;
+import pkgs.util.Util;
 
 public class Tst1 {
 
@@ -23,89 +24,13 @@ public class Tst1 {
 	}
 
 	private static void m2() {
-		System.out.println("testes com o hibernate...");
-
-		ObjetoX objetoX = null;
-
-		Configuration config = null;
-		SessionFactory sessionFactory = null;
-		Session session = null;
-		Transaction t = null;
-
-		try {
-
-			config = new Configuration();
-			config.configure();
-
-			sessionFactory = config.buildSessionFactory();
-			session = sessionFactory.openSession();
-
-			t = session.beginTransaction();
-
-			objetoX = new ObjetoX();
-			objetoX.setDescricao("Teste com hibernate descrição...");
-
-			session.save(objetoX);
-
-			t.commit();
-
-			session.close();
-
-			session = sessionFactory.openSession();
-
-			objetoX = session.find(ObjetoX.class, 1);
-
-			System.out.println("[objetoX=" + objetoX + "]");
-
-			session.close();
-			sessionFactory.close();
-
-			System.out.println("testes com o hibernate executados com sucesso...");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println("Tst1.m2()");
+		Util.testaHibernate();
 	}
 
 	private static void m3() {
-		System.out.println("testes com o JPA...");
-
-		ObjetoX objetoX = null;
-
-		EntityManagerFactory emf = null;
-		EntityManager em = null;
-		EntityTransaction et = null;
-
-		try {
-
-			emf = Persistence.createEntityManagerFactory("myPUCreate");
-			em = emf.createEntityManager();
-			et = em.getTransaction();
-
-			et.begin();
-
-			objetoX = new ObjetoX();
-			objetoX.setDescricao("Teste com JPA descrição...");
-
-			em.merge(objetoX);
-
-			et.commit();
-			em.close();
-			emf.close();
-
-			emf = Persistence.createEntityManagerFactory("myPUNoCreate");
-			em = emf.createEntityManager();
-
-			objetoX = em.find(ObjetoX.class, 1);
-
-			System.out.println("[objetoX=" + objetoX + "]");
-
-			em.close();
-			emf.close();
-
-			System.out.println("testes com o JPA executados com sucesso...");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println("Tst1.m3()");
+		Util.testaJPA();
 	}
 
 }
