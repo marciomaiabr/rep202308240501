561677907368f7ffe52c2a107d4d0cdf2858517c
Marcio Maia <marcio.luis.maia@gmail.com>
2023-08-26 10:36:05 -0400

    melhorias na aplicação dos teste

--- projects/prj202308240532/src/main/java/pkgs/tsts/Tst1.java
+++ projects/prj202308240532/src/main/java/pkgs/tsts/Tst1.java
@@ -1,5 +1,9 @@
 package pkgs.tsts;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+
 import pkgs.daos.PessoaDAO;
 import pkgs.models.Endereco;
 import pkgs.models.Pessoa;
@@ -8,24 +12,48 @@ import pkgs.util.Util;
 public class Tst1 {
 
 	public static void main(String[] args) {
-		m3();
+		try {
+			// System.out.println("Digite o Id do método que deseja executar:");
+			// BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			// String valorDigitado = reader.readLine();
+			String valorDigitado = javax.swing.JOptionPane.showInputDialog("Digite o Id do método que deseja executar:");
+			System.out.println("[valorDigitado="+valorDigitado+"]");
+			if(valorDigitado==null) {
+				System.out.println("Operação cancelada...");
+			}else {
+				Method[] methods = Tst1.class.getMethods();
+				Method method = null;
+				for(Method m : methods) {
+					if(m.getName().equals("m".concat(valorDigitado)))
+						method = m;
+				}
+				if(method==null) {
+					System.out.println("Método não encontrado...");
+				}else {
+					System.out.println("[method="+method+"]");
+					method.invoke(null);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	private static void m1() {
+	public static void m1() {
 		System.out.println("Olá mundo...");
 	}
 
-	private static void m2() {
+	public static void m2() {
 		System.out.println("Tst1.m2()");
 		Util.testaHibernate();
 	}
 
-	private static void m3() {
+	public static void m3() {
 		System.out.println("Tst1.m3()");
 		Util.testaJPA();
 	}
 
-	private static void m4() {
+	public static void m4() {
 		System.out.println("Tst1.m4()");
 		Pessoa pessoa = new Pessoa();
 		pessoa.setNome("lkj");
@@ -37,7 +65,7 @@ public class Tst1 {
 		pessoa.setEndereco(endereco);
 		endereco.setPessoa(pessoa);
 
-		if(! new PessoaDAO().salvar(pessoa)) {
+		if (!new PessoaDAO().salvar(pessoa)) {
 			Util.testaJPA();
 			new PessoaDAO().salvar(pessoa);
 		}
