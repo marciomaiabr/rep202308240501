3424e38fa7ac5f2189b5e3ac2c9ea9e70179e357
Marcio Maia <marcio.luis.maia@gmail.com>
2023-08-26 05:23:04 -0400

    criando teste de insert Pessoa

--- projects/prj202308240532/src/main/java/pkgs/tsts/Tst1.java
+++ projects/prj202308240532/src/main/java/pkgs/tsts/Tst1.java
@@ -1,11 +1,14 @@
 package pkgs.tsts;
 
+import pkgs.daos.PessoaDAO;
+import pkgs.models.Endereco;
+import pkgs.models.Pessoa;
 import pkgs.util.Util;
 
 public class Tst1 {
 
 	public static void main(String[] args) {
-		m3();
+		m4();
 	}
 
 	private static void m1() {
@@ -20,6 +23,24 @@ public class Tst1 {
 	private static void m3() {
 		System.out.println("Tst1.m3()");
 		Util.testaJPA();
+	}
+
+	private static void m4() {
+		System.out.println("Tst1.m4()");
+		Pessoa pessoa = new Pessoa();
+		pessoa.setNome("lkj");
+		pessoa.setSobreNome("kjh");
+
+		Endereco endereco = new Endereco();
+		endereco.setDescEndereco("jhg");
+
+		pessoa.setEndereco(endereco);
+		endereco.setPessoa(pessoa);
+
+		if(! new PessoaDAO().salvar(pessoa)) {
+			Util.testaJPA();
+			new PessoaDAO().salvar(pessoa);
+		}
 	}
 
 }
