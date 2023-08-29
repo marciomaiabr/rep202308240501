e394ce97fa313d9c45338a6018ad7bcede2e17a6
Marcio Maia <marcio.luis.maia@gmail.com>
2023-08-29 07:53:04 -0400

    Padronizando Util's

---
+++ projects/prj202308240532/src/main/java/pkgs/util/JPAUtil.java
@@ -0,0 +1,54 @@
+package pkgs.util;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+import pkgs.models.ObjetoX;
+
+public class JPAUtil {
+
+	public static void testaJPA() {
+		System.out.println("testes com o JPA...");
+
+		ObjetoX objetoX = null;
+
+		EntityManagerFactory emf = null;
+		EntityManager em = null;
+		EntityTransaction et = null;
+
+		try {
+
+			emf = Persistence.createEntityManagerFactory("myPUCreate");
+			em = emf.createEntityManager();
+			et = em.getTransaction();
+
+			et.begin();
+
+			objetoX = new ObjetoX();
+			objetoX.setDescricao("Teste com JPA descrição...");
+
+			em.merge(objetoX);
+
+			et.commit();
+			em.close();
+			emf.close();
+
+			emf = Persistence.createEntityManagerFactory("myPUNoCreate");
+			em = emf.createEntityManager();
+
+			objetoX = em.find(ObjetoX.class, 1);
+
+			System.out.println("[objetoX=" + objetoX + "]");
+
+			em.close();
+			emf.close();
+
+			System.out.println("testes com o JPA executados com sucesso...");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ projects/prj202308240532/src/main/java/pkgs/util/JSFUtil.java
@@ -0,0 +1,37 @@
+package pkgs.util;
+
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletRequest;
+
+public class JSFUtil {
+
+	public void listRequestParameters() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		Map<String, String> paramMap = context.getExternalContext().getRequestParameterMap();
+		Iterator<String> iter = paramMap.keySet().iterator();
+		while (iter.hasNext()) {
+			String paramKey = (String) iter.next();
+			System.out.println("paramKey = " + paramKey + " value = " + paramMap.get(paramKey).toString());
+		}
+	}
+
+	public static String buscaRequestParameterFormPrincipal(String parameterName) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		return context.getExternalContext().getRequestParameterMap().get("formPrincipal:".concat(parameterName));
+	}
+
+	public static String buscaRequestParameter(String parameterName) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		return context.getExternalContext().getRequestParameterMap().get(parameterName);
+	}
+
+	public static Object buscaRequestAttribute(String attributeName) {
+		FacesContext context = FacesContext.getCurrentInstance();
+		ServletRequest request = (ServletRequest) context.getExternalContext().getRequest();
+		return request.getAttribute(attributeName);
+	}
+
+}
