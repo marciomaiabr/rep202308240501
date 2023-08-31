ec891ec6a8d5d3c54f6faaa401d47145b691c71c
Marcio Maia <marcio.luis.maia@gmail.com>
2023-08-31 05:48:51 -0400

    adicionado relacionamento @OneToMany com a entidade SubAreaAtuacao

--- projects/prj202308240532/src/main/java/pkgs/models/AreaAtuacao.java
+++ projects/prj202308240532/src/main/java/pkgs/models/AreaAtuacao.java
@@ -1,10 +1,14 @@
 package pkgs.models;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class AreaAtuacao {
@@ -14,6 +18,8 @@ public class AreaAtuacao {
 	private Integer id;
 	@Column(length = 50)
 	private String descricao;
+	@OneToMany(mappedBy = "areaAtuacao", cascade = CascadeType.ALL)
+	private List<SubAreaAtuacao> subAreaAtuacao;
 
 	public Integer getId() {
 		return id;
@@ -29,6 +35,14 @@ public class AreaAtuacao {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public List<SubAreaAtuacao> getSubAreaAtuacao() {
+		return subAreaAtuacao;
+	}
+
+	public void setSubAreaAtuacao(List<SubAreaAtuacao> subAreaAtuacao) {
+		this.subAreaAtuacao = subAreaAtuacao;
 	}
 
 	@Override
