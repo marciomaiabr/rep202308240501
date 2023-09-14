69c51505a79ff592982573cdded9a8ca2c3f8cd1
Marcio Maia <marcio.luis.maia@gmail.com>
2023-09-14 04:44:19 -0400

    Adicionando novos atributos a entidade Endereco

--- projects/prj202308240532/src/main/java/pkgs/models/Endereco.java
+++ projects/prj202308240532/src/main/java/pkgs/models/Endereco.java
@@ -15,8 +15,20 @@ public class Endereco {
 	private Integer id;
 	@OneToOne
 	private Pessoa pessoa;
-	@Column(length = 75)
-	private String descEndereco;
+	@Column(length = 25)
+	private String rua;
+	@Column(length = 10)
+	private String numero;
+	@Column(length = 25)
+	private String complemento;
+	@Column(length = 25)
+	private String bairro;
+	@Column(length = 25)
+	private String cidade;
+	@Column(length = 9)
+	private String uf;
+	@Column(length = 110)
+	private String enderecoCompleto;
 
 	public Integer getId() {
 		return id;
@@ -34,22 +46,67 @@ public class Endereco {
 		this.pessoa = pessoa;
 	}
 
-	public String getDescEndereco() {
-		return descEndereco;
+	public String getRua() {
+		return rua;
 	}
 
-	public void setDescEndereco(String descEndereco) {
-		this.descEndereco = descEndereco;
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getEnderecoCompleto() {
+		return enderecoCompleto;
+	}
+
+	public void setEnderecoCompleto(String enderecoCompleto) {
+		this.enderecoCompleto = enderecoCompleto;
 	}
 
 	@Override
 	public String toString() {
-		return "Endereco [id=" + id + ", descEndereco=" + descEndereco + "]";
-	}
-
-	public Endereco() {
-		super();
-		System.out.println("Endereco.Endereco()");
+		return "Endereco [id=" + id + ", rua=" + rua + ", numero=" + numero + ", complemento=" + complemento
+				+ ", bairro=" + bairro + ", cidade=" + cidade + ", uf=" + uf + ", enderecoCompleto=" + enderecoCompleto
+				+ "]";
 	}
 
 }
