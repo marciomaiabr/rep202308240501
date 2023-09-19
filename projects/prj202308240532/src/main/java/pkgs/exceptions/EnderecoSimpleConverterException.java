05c423ce31bd5510b0fdcb7ce7b3af14c5c9f96b
Marcio Maia <marcio.luis.maia@gmail.com>
2023-09-19 04:59:43 -0400

    Criando Exception para convert de Endereco

---
+++ projects/prj202308240532/src/main/java/pkgs/exceptions/EnderecoSimpleConverterException.java
@@ -0,0 +1,15 @@
+package pkgs.exceptions;
+
+public class EnderecoSimpleConverterException extends RuntimeException {
+
+	private static final long serialVersionUID = 7537808452390245987L;
+
+	public EnderecoSimpleConverterException() {
+		super();
+	}
+
+	public EnderecoSimpleConverterException(String message) {
+		super(message);
+	}
+	
+}
