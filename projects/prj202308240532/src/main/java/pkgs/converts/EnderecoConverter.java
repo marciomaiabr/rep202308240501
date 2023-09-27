45c5875fd9e81695ca2cf50051b2c550343f0fcc
Marcio Maia <marcio.luis.maia@gmail.com>
2023-09-27 03:54:32 -0400

    Criando converter para Endereco

---
+++ projects/prj202308240532/src/main/java/pkgs/converts/EnderecoConverter.java
@@ -0,0 +1,68 @@
+package pkgs.converts;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+
+import pkgs.exceptions.EnderecoConverterException;
+import pkgs.models.Endereco;
+import pkgs.repositorys.Repositorys;
+import pkgs.util.JSFUtil;
+
+//@FacesConverter("enderecoConverter")
+public class EnderecoConverter implements Converter {
+
+	@Override
+	public Object getAsObject(FacesContext context, UIComponent component, String value) {
+		System.out.println("EnderecoConverter.getAsObject()");
+		System.out.println("[value=" + value + "]");
+
+		Endereco endereco = null;
+
+		String[] aSEndereco = value.split(";");
+
+		String idPrincipal = JSFUtil.buscaRequestParameter("idPrincipal");
+
+		if (idPrincipal != null)
+			endereco = Repositorys.getRepository().buscaPorId(Integer.parseInt(idPrincipal)).getEndereco();
+		else
+			endereco = new Endereco();
+
+		if (aSEndereco.length == 5) {
+			String[] aSCidadeUf = aSEndereco[4].split("-");
+			if (aSCidadeUf.length == 2) {
+				endereco.setRua(aSEndereco[0]);
+				endereco.setNumero(aSEndereco[1]);
+				endereco.setComplemento(aSEndereco[2]);
+				endereco.setBairro(aSEndereco[3]);
+				endereco.setCidade(aSCidadeUf[0]);
+				endereco.setUf(aSCidadeUf[1]);
+			}
+		}
+
+		System.out.println("[endereco=" + endereco + "]");
+
+		return endereco;
+	}
+
+	@Override
+	public String getAsString(FacesContext context, UIComponent component, Object value) {
+		System.out.println("EnderecoConverter.getAsString()");
+		System.out.println("[value=" + value + "]");
+
+		Endereco endereco = (Endereco) value;
+
+		String sEndereco = "";
+		sEndereco += endereco.getRua() + ";" + endereco.getNumero() + ";" + endereco.getComplemento() + ";"
+				+ endereco.getBairro() + ";";
+		sEndereco += endereco.getCidade() + "-" + endereco.getUf();
+
+		System.out.println("[sEndereco=" + sEndereco + "]");
+
+		return sEndereco;
+	}
+
+}
