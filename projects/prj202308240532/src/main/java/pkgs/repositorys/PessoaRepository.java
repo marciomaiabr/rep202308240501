5affe0e4a6cc005b0dfad6788923b11a88d71efc
Marcio Maia <marcio.luis.maia@gmail.com>
2023-10-07 05:18:06 -0400

    adicionado métodos getSubAreaAtuacaoById / listarSubAreasAtuacao

--- projects/prj202308240532/src/main/java/pkgs/repositorys/PessoaRepository.java
+++ projects/prj202308240532/src/main/java/pkgs/repositorys/PessoaRepository.java
@@ -3,10 +3,13 @@ package pkgs.repositorys;
 import java.util.List;
 
 import pkgs.models.AreaAtuacao;
+import pkgs.models.SubAreaAtuacao;
 
 public interface PessoaRepository extends Repository {
 
 	public List<AreaAtuacao> listarAreasAtuacao();
 	public AreaAtuacao getAreaAtuacaoById(Integer id);
+	public SubAreaAtuacao getSubAreaAtuacaoById(Integer id);
+	public List<SubAreaAtuacao> listarSubAreasAtuacao();
 
 }
